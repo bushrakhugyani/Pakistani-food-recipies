54e6bea4e4b42d5bca5c15d573e0c3eb1185ac7a
Bushra Khugyani <bushrakhugyani@gmail.com>
2024-03-01 18:52:55 -0800

    added some JS to ask API to generate recipies also added axios file to the html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-function generateRecipe(event) {
-  event.preventDefault();
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "👉The recipy will display here 🥘🍲🍛",
+    strings: response.data.answer,
     autoStart: true,
     delay: 80,
     cursor: null,
   });
+}
+function generateRecipe(event) {
+  event.preventDefault();
+  let recipeNameInput = document.querySelector("#recipe-name");
+  let apiKey = "98f12063t8f0b4be43fb6oa12441998c";
+  let prompt = `Generate a Pakistani food recipe of ${recipeNameInput.value}`;
+  let context =
+    "Generate a Pakistani food recipe in short sentences on the bases of the user instruction in input and display the answer in HTML <ul> format";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayRecipe);
 }
 let recipeFormElement = document.querySelector("#recipe-generator-form");
 recipeFormElement.addEventListener("submit", generateRecipe);
