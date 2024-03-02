f46021dc61b8dff99e7ea611c9a4f50e2ef6f3ee
Bushra Khugyani <bushrakhugyani@gmail.com>
2024-03-01 17:48:51 -0800

    added basic form handling and also added typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-
+function generateRecipe(event) {
+  event.preventDefault();
+  new Typewriter("#recipe", {
+    strings: "👉The recipy will display here 🥘🍲🍛",
+    autoStart: true,
+    delay: 80,
+    cursor: null,
+  });
+}
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
