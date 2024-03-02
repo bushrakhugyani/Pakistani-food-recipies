1256ea2801ca6b97459fc0335444a106a8eda105
Bushra Khugyani <bushrakhugyani@gmail.com>
2024-03-01 20:35:23 -0800

    added some more HTML and some CSS and JavaScript for the user comfort.

--- src/index.js
+++ src/index.js
@@ -12,8 +12,12 @@ function generateRecipe(event) {
   let apiKey = "98f12063t8f0b4be43fb6oa12441998c";
   let prompt = `Generate a Pakistani food recipe of ${recipeNameInput.value}`;
   let context =
-    "Generate a Pakistani food recipe in short sentences on the bases of the user instruction in input and display the answer in HTML <ul> format";
+    "Generate a Pakistani food recipe in short sentences on the bases of the user instruction in input and display the answer in HTML numbers format,   Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">⏳ Generating Pakistani food recipe of ${recipeNameInput.value}</div>`;
   axios.get(apiUrl).then(displayRecipe);
 }
 let recipeFormElement = document.querySelector("#recipe-generator-form");
